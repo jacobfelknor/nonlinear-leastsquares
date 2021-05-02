011eac8e47ebce1ab588aca0ecbf2c6683cf6af2
jamescarley1 <70235291+jamescarley1@users.noreply.github.com>
2021-05-02 13:02:49 -0600

    Add files via upload

--- world.m
+++ world.m
@@ -1,3 +1,4 @@
+close all;
 clear;
 clc;
 format long;
@@ -8,11 +9,13 @@ N = size(data, 1);          % number of samples
 A = zeros(N, 3);            % preallocate some matricies
 R = zeros(N, 1);            % "  "
 YEAR_MIN = data(1:1);
+YEAR_MAX = 2100;
 % initial guesses
 q_prime = 1500000; % times 10^3 for final ans
 sigma_prime = 40;
 mu_prime = 2000;
 na = 0;
+
 % single iteration for now, but will iterate until convergence eventually
 iterations = 1000;
 for dummy = 1:iterations
@@ -39,3 +42,23 @@ for dummy = 1:iterations
     sigma_prime = sigma_prime + delta(2);
     mu_prime = mu_prime + delta(3);
 end
+
+% check when production falls beneath 5%
+production_curve = q(YEAR_MIN:YEAR_MAX, q_prime, sigma_prime, mu_prime);
+max_production = max(production_curve);
+five_percent = max_production * 0.05;
+run_out_index = find(flip(production_curve) > five_percent, 1);
+actual_year = YEAR_MAX - run_out_index;
+x = YEAR_MIN:YEAR_MAX;
+figure
+plot(x, production_curve);
+xlabel('Year')
+ylabel('Oil production (millons of barrels)')
+hold on
+q(mu_prime, q_prime, sigma_prime, mu_prime)
+plot(mu_prime, q(mu_prime, q_prime, sigma_prime, mu_prime), 'r*');
+str = sprintf(' Max Production: mu=%f', mu_prime);
+text(mu_prime, q(mu_prime, q_prime, sigma_prime, mu_prime), str);
+plot(actual_year, q(actual_year, q_prime, sigma_prime, mu_prime), 'r*');
+str = sprintf(' "Ran out" at year %d', actual_year);
+text(2020, 0.5e6, str);
